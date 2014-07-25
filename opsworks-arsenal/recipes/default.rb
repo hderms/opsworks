c0ed4d679f8b5526fa5426b7708acb8ea8fc8091
Jon Boydell <jon.boydell@akqa.com>
2014-07-25 15:11:18 +0100

    missed current dir

--- opsworks-arsenal/recipes/default.rb
+++ opsworks-arsenal/recipes/default.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
 
-	template "#{deploy[:deploy_to]}/config/connections.js" do
+	template "#{deploy[:deploy_to]}/current/config/connections.js" do
   		source "connections_js.erb"
   		mode 00700
   		owner "root"
